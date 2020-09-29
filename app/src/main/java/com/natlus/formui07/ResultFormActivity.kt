a3fcb4570ec046b725fd12be6a7343bf2ee9b412
Sultan Achmad <sultangendut@gmail.com>
2020-09-29 11:26:55 +0700

    initial commit

---
+++ app/src/main/java/com/natlus/formui07/ResultFormActivity.kt
@@ -0,0 +1,11 @@
+package com.natlus.formui07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_form)
+    }
+}
