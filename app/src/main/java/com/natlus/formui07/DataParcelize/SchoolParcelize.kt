d3744574502e588b03bca7caf59d1c7630115f9f
Sultan Achmad Qum Masykuro NS <sultangendut@gmail.com>
2020-10-01 19:10:34 +0700

    add School Parcelize and init intent to Result Form (#3)

---
+++ app/src/main/java/com/natlus/formui07/DataParcelize/SchoolParcelize.kt
@@ -0,0 +1,19 @@
+package com.natlus.formui07.DataParcelize
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SchoolParcelize(
+    var namaUnivAsal:String,
+    var namaFakultasAsal:String,
+    var namaProdiAsal:String,
+    var provinsiUnivAsal:String,
+    var kotaUnivAsal:String,
+    var alamatUnivAsal:String,
+    var kodePosUnivAsal:String,
+    var akreditasiUnivAsal:String,
+    var nilaiIPK:String,
+    var dataPribadi: PribadiParcelize,
+    var dataParent: ParentParcelize
+) : Parcelable
