ee4f1bde9389834fa5cf86ed89cfbe7d91bd104b
Sultan Achmad Qum Masykuro NS <sultangendut@gmail.com>
2020-10-01 18:55:08 +0700

    add Parent Parcelize and init intent to School Form (#2)

--- app/src/main/java/com/natlus/formui07/ParentFormActivity.kt
+++ app/src/main/java/com/natlus/formui07/ParentFormActivity.kt
@@ -1,22 +1,75 @@
 package com.natlus.formui07
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.natlus.formui07.DataParcelize.ParentParcelize
 import com.natlus.formui07.DataParcelize.PribadiParcelize
 
 class ParentFormActivity : AppCompatActivity() {
     private val DATA_PRIBADI = "DATA_PRIBADI"
+    private val DATA_PARENT = "DATA_PARENT"
+
+    private lateinit var namaAyah: String
+    private var nikAyah: Int = 0
+    private lateinit var namaIbu: String
+    private var nikIbu: Int = 0
+    private lateinit var tanggalLahirAyah: String
+    private lateinit var tanggalLahirIbu: String
+    private lateinit var alamatParent: String
+    private lateinit var kodePos: String
+    private lateinit var phoneOrtu: String
+    private lateinit var pendidikanAyah: String
+    private lateinit var pendidikanIbu: String
+    private lateinit var pekerjaanAyah: String
+    private lateinit var pekerjaanIbu: String
+    private lateinit var dataPribadi: PribadiParcelize
+
+    private lateinit var btnParenttoSchool: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parent_form)
 
+        setUp()
         getDataPribadi()
+
+        btnParenttoSchool.setOnClickListener {
+            intentToSchool()
+        }
+    }
+
+    private fun setUp() {
+        btnParenttoSchool = findViewById(R.id.btnParenttoSchool)
     }
 
     private fun getDataPribadi() {
         val dataPribadi = intent.getParcelableExtra<PribadiParcelize>(DATA_PRIBADI)
         Log.e(DATA_PRIBADI, dataPribadi.toString())
+    }
+
+    private fun intentToSchool() {
+        val dataParent = ParentParcelize(
+            namaAyah,
+            nikAyah,
+            namaIbu,
+            nikIbu,
+            tanggalLahirAyah,
+            tanggalLahirIbu,
+            alamatParent,
+            kodePos,
+            phoneOrtu,
+            pendidikanAyah,
+            pendidikanIbu,
+            pekerjaanAyah,
+            pekerjaanIbu,
+            dataPribadi
+        )
+
+        val resultIntent = Intent(this, SchoolFormActivity::class.java)
+        resultIntent.putExtra(DATA_PARENT, dataParent)
+        startActivity(resultIntent)
     }
 }
